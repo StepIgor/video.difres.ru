79d762c35dd4fd446264c19f9866a706b69ca0c4
Igor Stepanov <stepanow.igor@gmail.com>
2022-04-10 21:09:42 +0300

    Remove videos

--- src/db.js
+++ src/db.js
@@ -1,34 +1,18 @@
 export let videos = {
     1:{
         "channel": 			1,
-        "name": 			"Fortnite highlight | Первое место (#1)",
-        "date": 			"18.04.2021 21:30",
-        "description": 		"Записано с использованием технологии NVIDIA highlights Auto Capture",
-        "duration": 		"0:20",
-        "theme": 			1
-    },
-    2:{
-        "channel": 			1,
-        "name": 			"Fortnite highlight | Первое место (#2)",
+        "name": 			"Пустой видеоролик",
         "date": 			"19.04.2021 21:42",
-        "description": 		"Записано с использованием технологии NVIDIA highlights Auto Capture",
-        "duration": 		"0:20",
+        "description": 		"Бессмысленный набор слов в качестве описания пустого видеоролика",
+        "duration": 		"0:10",
         "theme": 			1
-    },
-    3:{
-        "channel":          1,
-        "name":             "CS:GO | Эйс (#1)",
-        "date":             "27.07.2021 16:29",
-        "description":      "Игра на повышение звания, момент из демонстрации",
-        "duration":         "1:10",
-        "theme":            2
     }
 }
 
 export let users = {
     1:{
         "name": 			"Darkness",
-        "description": 		"Компьютерные игры и всё, что с ними связано",
+        "description": 		"Канал для публикации видеороликов на разные темы",
         "creation_date": 	"18.04.2021",
         "verified": 		true
     }
@@ -39,11 +23,10 @@ export let comments = {
         "author":			1,
         "related video":	1,
         "date":				"18.04.2021 21:31",
-        "content":			"Первый комментарий к первому видео ;)"
+        "content":			"Первый комментарий к первому видео :)"
     }
 }
 
 export let themes = {
-    1: "Fortnite",
-    2: "Counter-Strike: Global Offensive"
+    1: "Категория тестирования",
 }
